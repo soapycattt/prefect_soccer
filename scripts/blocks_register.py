3496f4d93065d8add3f09380e2ec5c4767cb7a08
Triet Le <triet.lq@holistics.io>
2023-04-02 15:43:02 +0700

    update script to register blocks

--- scripts/blocks_register.py
+++ scripts/blocks_register.py
@@ -3,14 +3,15 @@ from prefect_gcp import GcpCredentials, GcsBucket
 import os
 
 gcp_cred_path = os.getenv("GCP_CRED_PATH")
+bucket = os.getenv("BUCKET")
 
-GcpCredentials(
-    service_account_file=gcp_cred_path
-).save("decamp-gcp-cred", overwrite=True)
+GcpCredentials(service_account_file=gcp_cred_path).save(
+    "decamp-gcp-cred", overwrite=True
+)
 
 gcp_credential = GcpCredentials.load("decamp-gcp-cred")
 
 GcsBucket(
-    bucket="dzc-trietle-data-lake",
+    bucket=bucket,
     gcp_credentials=gcp_credential,
 ).save(name="decamp-bucket", overwrite=True)
